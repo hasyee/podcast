c3c1437af6fabf8546b7936ac0bac565755c5012
KriszKecskes <git.flatware971@passinbox.com>
2024-06-17 14:01:26 +0200

    Fix responsive grid

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useSearch, useSearchResult } from './hooks';
 import { useStorage } from './hooks/storage';
 import { Episode } from './types';
 import CustomAppShell from '../src/components/CustomAppShell';
-import { Box, Card, Group, Title, Image, Button, Text, Grid } from '@mantine/core';
+import { Box, Card, Group, Title, Image, Button, Text, Grid, Container } from '@mantine/core';
 
 let audio: HTMLAudioElement;
 
@@ -84,115 +84,125 @@ function App() {
 
   return (
     <CustomAppShell>
-      <div className="App">
-        <header>
-          {playingId && (
-            <div>
-              <span>{rate}</span>
-              <input type="range" min={0} max={2} step={0.1} value={rate} onChange={handleRateChange} />
-            </div>
-          )}
-          {duration && (
-            <div>
-              <span>
-                {time} / {duration}
-                {<input type="range" min={0} max={duration} step={1} value={time} onChange={handleTimeChange} />}
-              </span>
-            </div>
-          )}
-        </header>
+      <Container>
+        <div className="App">
+          <header>
+            {playingId && (
+              <div>
+                <span>{rate}</span>
+                <input type="range" min={0} max={2} step={0.1} value={rate} onChange={handleRateChange} />
+              </div>
+            )}
+            {duration && (
+              <div>
+                <span>
+                  {time} / {duration}
+                  {<input type="range" min={0} max={duration} step={1} value={time} onChange={handleTimeChange} />}
+                </span>
+              </div>
+            )}
+          </header>
 
-        <Box>
-          <Title mb={'md'} order={2}>
-            Channels
-          </Title>
+          <Box>
+            <Title mb={'md'} order={2}>
+              Channels
+            </Title>
 
-          <Group>
-            {searchResult.channels.map(channel => (
-              <Card key={channel.id} shadow="sm" padding="lg" radius="md" withBorder>
-                <Card.Section>
-                  <Image w={220} src={channel.imageUrl} fit="contain" alt="Norway" />
-                </Card.Section>
+            <Group>
+              {searchResult.channels.map(channel => (
+                <Card key={channel.id} shadow="sm" padding="lg" radius="md" withBorder>
+                  <Card.Section>
+                    <Image w={220} src={channel.imageUrl} fit="contain" alt="Norway" />
+                  </Card.Section>
 
-                <Group justify="space-between" mt="md" mb="xs">
-                  <Text fw={500}>{channel.name}</Text>
-                </Group>
+                  <Group justify="space-between" mt="md" mb="xs">
+                    <Text fw={500}>{channel.name}</Text>
+                  </Group>
 
-                <Text size="sm" c="dimmed">
-                  From: {channel.creatorName}
-                </Text>
-                <Text size="sm" c="dimmed">
-                  Episodes: {channel.episodeCount}
-                </Text>
+                  <Text size="sm" c="dimmed">
+                    From: {channel.creatorName}
+                  </Text>
+                  <Text size="sm" c="dimmed">
+                    Episodes: {channel.episodeCount}
+                  </Text>
 
-                <Button component="a" target="_blank" href={channel.feedUrl} color="blue" fullWidth mt="md" radius="md">
-                  Feed URL
-                </Button>
-              </Card>
-            ))}
-          </Group>
-        </Box>
+                  <Button
+                    component="a"
+                    target="_blank"
+                    href={channel.feedUrl}
+                    color="blue"
+                    fullWidth
+                    mt="md"
+                    radius="md"
+                  >
+                    Feed URL
+                  </Button>
+                </Card>
+              ))}
+            </Group>
+          </Box>
 
-        <Box>
-          <Title mt={'md'} order={2}>
-            Episodes
-          </Title>
+          <Box>
+            <Title mt={'md'} order={2}>
+              Episodes
+            </Title>
 
-          <Grid mt="md">
-            {searchResult.episodes.map(episode => {
-              const duration = moment.duration(episode.duration, 'seconds');
-              return (
-                <Grid.Col key={episode.id} span={{ base: 12, sm: 6, lg: 3 }}>
-                  <Card shadow="sm" padding="lg" radius="md" withBorder>
-                    <Card.Section>
-                      <Image src={episode.imageUrl} fit="contain" alt="Norway" />
-                    </Card.Section>
+            <Grid mt="md">
+              {searchResult.episodes.map(episode => {
+                const duration = moment.duration(episode.duration, 'seconds');
+                return (
+                  <Grid.Col key={episode.id} span={{ base: 12, sm: 6, lg: 3 }}>
+                    <Card shadow="sm" padding="lg" radius="md" withBorder>
+                      <Card.Section>
+                        <Image src={episode.imageUrl} fit="contain" alt="Norway" />
+                      </Card.Section>
 
-                    <Group justify="space-between" mt="md" mb="xs">
-                      <Text fw={500}>
-                        {episode.channelName} - {episode.name}
+                      <Group justify="space-between" mt="md" mb="xs">
+                        <Text fw={500}>
+                          {episode.channelName} - {episode.name}
+                        </Text>
+                      </Group>
+
+                      <Text size="sm" c="dimmed">
+                        Release date: {episode.releaseDate}
                       </Text>
-                    </Group>
+                      <Text size="sm" c="dimmed">
+                        Duration: {duration.hours()}:{duration.minutes()}:{duration.seconds()}
+                      </Text>
 
-                    <Text size="sm" c="dimmed">
-                      Release date: {episode.releaseDate}
-                    </Text>
-                    <Text size="sm" c="dimmed">
-                      Duration: {duration.hours()}:{duration.minutes()}:{duration.seconds()}
-                    </Text>
+                      <Text mt={'sm'} size="sm" truncate="end" title={episode.description}>
+                        {episode.description}
+                      </Text>
 
-                    <Text mt={'sm'} size="sm" truncate="end" title={episode.description}>
-                      {episode.description}
-                    </Text>
+                      <Group mt="md">
+                        <audio controls style={{ width: '100%' }}>
+                          <source src={episode.audioUrl} type="audio/mpeg" />
+                          Your browser does not support the audio element.
+                        </audio>
+                      </Group>
 
-                    <Group mt="md">
-                      <audio controls style={{ width: '100%' }}>
-                        <source src={episode.audioUrl} type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                      </audio>
-                    </Group>
-
-                    <Group mt={'md'}>
-                      <Button
-                        onClick={() => {
-                          downloadedIds.includes(episode.id) ? removeEpisode(episode.id) : saveEpisode(episode);
-                        }}
-                      >
-                        {downloadedIds.includes(episode.id) ? 'Remove' : 'Download'}
-                      </Button>
-                      {downloadedIds.includes(episode.id) && (
-                        <Button onClick={() => (playingId === episode.id ? stop() : loadEpisode(episode.id))}>
-                          {playingId === episode.id ? 'Pause' : 'Play'} downloaded
+                      <Group mt={'md'}>
+                        <Button
+                          onClick={() => {
+                            downloadedIds.includes(episode.id) ? removeEpisode(episode.id) : saveEpisode(episode);
+                          }}
+                        >
+                          {downloadedIds.includes(episode.id) ? 'Remove' : 'Download'}
                         </Button>
-                      )}
-                    </Group>
-                  </Card>
-                </Grid.Col>
-              );
-            })}
-          </Grid>
-        </Box>
-      </div>
+                        {downloadedIds.includes(episode.id) && (
+                          <Button onClick={() => (playingId === episode.id ? stop() : loadEpisode(episode.id))}>
+                            {playingId === episode.id ? 'Pause' : 'Play'} downloaded
+                          </Button>
+                        )}
+                      </Group>
+                    </Card>
+                  </Grid.Col>
+                );
+              })}
+            </Grid>
+          </Box>
+        </div>
+      </Container>
     </CustomAppShell>
   );
 }
